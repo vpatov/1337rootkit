254058e39dace35bcc95da16a577d6dad9fc7538
Edwin Yan <edwin@edwin-VirtualBox.(none)>
2016-11-27 13:34:19 -0500

    syntax errors fixed

--- .1337rootkit.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/edwin/1337rootkit/1337rootkit.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.11.0-15-generic/scripts/module-common.lds --build-id  -o /home/edwin/1337rootkit/1337rootkit.ko /home/edwin/1337rootkit/1337rootkit.o /home/edwin/1337rootkit/1337rootkit.mod.o
